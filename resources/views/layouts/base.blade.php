c325e96c64d675749fce98c5d57beb9d28cab334
mkstudio <marlina.kratif@gmail.com>
2025-01-29 18:40:50 +0700

    update menu and style

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -64,7 +64,7 @@
     <!-- Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <!-- CSS Files -->
-    <link id="pagestyle" href="{{ asset('assets') }}/css/material-dashboard.css?v=3.0.0" rel="stylesheet" />
+    <link id="pagestyle" href="{{ asset('assets') }}/css/material-dashboard.css?v=3.0.1" rel="stylesheet" />
     @livewireStyles
 </head>
 
